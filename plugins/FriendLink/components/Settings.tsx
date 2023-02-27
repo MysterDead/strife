8125bf3356068c9e4eb0a020c4d76162fc8ced03
MysterDead <bobryk20@gmail.com>
2023-02-28 00:49:20 +0100

    update settings

--- plugins/FriendLink/components/Settings.tsx
+++ plugins/FriendLink/components/Settings.tsx
@@ -1,12 +1,36 @@
 /// <reference path="..\..\..\node_modules\vendetta-types\defs.d.ts" />
 
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { ReactNative as RN, url, invites } from "@vendetta/metro/common";
+import {ErrorBoundary, Forms} from "@vendetta/ui/components";
+import {getAssetIDByName} from "@vendetta/ui/assets";
+const { FormText, FormSection, FormSwitchRow, FormRow } = Forms;
+import settings from "@vendetta/storage";
 
 export default () => (
     <>
-        <FormText>
-            Friend Invite Links
-        </FormText>
+        <ErrorBoundary>
+            <RN.ScrollView style={{flex: 1}} contentContainerStyle={{paddingBottom: 38}}>
+                <FormSection title="Show in" titleStyleType="no_border">
+                    <FormSwitchRow
+                        label="User profile"
+                        subLabel="Show invite button in user profile. When you click you send a friend invite link to selected user as message"
+                        leading={<FormRow.Icon source={getAssetIDByName("ic_members")} />}
+                        value={false}
+                        onValueChange={(v: boolean) => {
+                            //change
+                        }}
+                    />
+                    <FormSwitchRow
+                        label="Friend Tab"
+                        subLabel="Show invite button as toolbox in friends tab"
+                        leading={<FormRow.Icon source={getAssetIDByName("friends_toast_icon")} />}
+                        value={false}
+                        onValueChange={(v: boolean) => {
+                            //change
+                        }}
+                    />
+                </FormSection>
+            </RN.ScrollView>
+        </ErrorBoundary>
     </>
 )
