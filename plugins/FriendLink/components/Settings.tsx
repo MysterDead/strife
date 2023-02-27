2a53631ebbe88c2ef6a16c771e78c7e3743eab77
MysterDead <bobryk20@gmail.com>
2023-02-28 00:25:25 +0100

    first

---
+++ plugins/FriendLink/components/Settings.tsx
@@ -0,0 +1,10 @@
+/// <reference path="..\..\..\node_modules\vendetta-types\defs.d.ts" />
+
+import { Forms } from "@vendetta/ui/components";
+const { FormText } = Forms;
+
+export default () => (
+    <FormText>
+        Hello, world!
+    </FormText>
+)
---
+++ plugins/FriendLink/index.ts
@@ -0,0 +1,12 @@
+import { logger } from "@vendetta";
+import Settings from "./components/Settings";
+
+export default {
+    onLoad: () => {
+        logger.log("Hello world!");
+    },
+    onUnload: () => {
+        logger.log("Goodbye, world.");
+    },
+    settings: Settings,
+}
