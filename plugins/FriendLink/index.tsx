21972e42fc8fffdf5d3ccfd726514b9aa7c2dc3c
MysterDead <bobryk20@gmail.com>
2023-02-28 02:25:49 +0100

    test2

--- plugins/FriendLink/index.tsx
+++ plugins/FriendLink/index.tsx
@@ -3,17 +3,21 @@ import Settings from "./components/Settings";
 import { storage } from "@vendetta/plugin";
 import {findByDisplayName} from "@vendetta/metro";
 import {after} from "@vendetta/patcher";
+import {Forms} from "@vendetta/ui/components";
 
 storage.profileButton ??=false;
 storage.friendsTabButton ??=false;
 let unpatch;
 
-const UserProfile = findByDisplayName("UserProfile");
+const UserProfile = findByDisplayName("UserProfileRelations");
 export default {
     onLoad: () => {
         logger.log("Hello world!");
-        unpatch = after("default", UserProfile, ([{ userNode }], res) => {
-            logger.log(JSON.stringify(userNode));
+        unpatch = after("render", UserProfile, ([{ userNode }], res) => {
+            res.children.push(<Forms.FormText>TESTMODE</Forms.FormText>)
+        });
+        after("default", UserProfile, ([{ userNode }], res) => {
+            res.children.push(<Forms.FormText>TESTMODE</Forms.FormText>)
         });
     },
     onUnload: () => {
