b2d9a137eeb4ad5b23bbb088cb39daa748f228b0
MysterDead <bobryk20@gmail.com>
2023-02-28 14:24:38 +0100

    test

--- plugins/FriendLink/index.tsx
+++ plugins/FriendLink/index.tsx
@@ -14,15 +14,22 @@ const UserProfileRelations = findByDisplayName("UserProfileRelations", false);
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 const Icon = findByDisplayName("Icon");
 
+const {
+    default: UserProfileRow,
+    label,
+    onPress,
+    trailing,
+} = findByProps("label", "onPress", "trailing");
+
 const UnpatchRelations = after('default', UserProfileRelations, ([{hideUserProfile, showUserProfile, user}], res) => {
-    let buttons = res?.props?.children;
+    let buttons = res?.props?.children?.find(e=> e.type.name === 'UserProfileSection')?.children;
     const buttonIcon = getAssetIDByName("ic_select_manually_24px");
     const buttonCallback = () => {
         console.log("I was clicked!");
         // You can use LazyActionSheet's hideActionSheet function to close the action sheet.
         LazyActionSheet.hideActionSheet();
     };
-    buttons.push((<Forms.FormRow
+    buttons.push((<UserProfileRow
         trailing={<Icon source={buttonIcon} />}
         label={'Send Friend Invite link'}
         onPress={buttonCallback}
