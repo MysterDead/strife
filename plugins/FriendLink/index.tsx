8a2181e03a235304d4bf112c85a8aec0b03dc851
MysterDead <bobryk20@gmail.com>
2023-02-28 17:03:58 +0100

    in relations show button

--- plugins/FriendLink/index.tsx
+++ plugins/FriendLink/index.tsx
@@ -1,7 +1,8 @@
 import { logger } from "@vendetta";
 import Settings from "./components/Settings";
 import { storage } from "@vendetta/plugin";
-import {findByDisplayName, findByProps, } from "@vendetta/metro";
+import {findByDisplayName, findByProps} from "@vendetta/metro";
+import {ReactNative as RN } from '@vendetta/metro/common';
 import {after} from "@vendetta/patcher";
 import {Forms, General} from "@vendetta/ui/components";
 import {getAssetByID} from "@vendetta/ui/assets";
@@ -9,17 +10,19 @@ import {getAssetByID} from "@vendetta/ui/assets";
 storage.profileButton ??=false;
 storage.friendsTabButton ??=false;
 
-const UserProfileExperimentWrapper = findByDisplayName("UserProfileRelations", false);
-const Icon = findByDisplayName("Icon");
+const UserProfileRelations = findByDisplayName("UserProfileRelations", false);
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 
 
-const UnpatchRelations = after('default', UserProfileExperimentWrapper, (ctx, component) => {
+const UnpatchRelations = after('default', UserProfileRelations, (ctx, component) => {
     const { props } = component;
     const { children } = props;
     if(children === undefined) return;
+    logger.log("MysterDead - I see child widget");
     const buttons = children?.props?.children[1]?.props?.children;
+    logger.log("MysterDead - Try to see buttons");
     if(buttons === undefined) return;
+    logger.log("MysterDead - I see buttons");
     const buttonCallback = () => {
         console.log("I was clicked!");
         LazyActionSheet.hideActionSheet();
@@ -28,7 +31,7 @@ const UnpatchRelations = after('default', UserProfileExperimentWrapper, (ctx, co
     buttons.push((<Forms.FormRow
         label={'Send Friend Invite link'}
         onPress={buttonCallback}
-        trailing={<Icon source={105} size={'medium'} disableColor={false}/>}
+        trailing={<Forms.FormRow.Icon source={getAssetByID(105)} size={'medium'} disableColor={false}/>}
     />));
     // @ts-ignore
     ctx.result = [component]
