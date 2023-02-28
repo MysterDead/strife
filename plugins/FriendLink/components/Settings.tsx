75b0323714b2bc08b7bec2114d43290f00a774e5
MysterDead <bobryk20@gmail.com>
2023-02-28 01:19:29 +0100

    fix

--- plugins/FriendLink/components/Settings.tsx
+++ plugins/FriendLink/components/Settings.tsx
@@ -5,25 +5,19 @@ import {ErrorBoundary, Forms} from "@vendetta/ui/components";
 import {getAssetIDByName} from "@vendetta/ui/assets";
 const { FormSection, FormSwitchRow, FormRow } = Forms;
 
-export default function Settings() {
-    return (
-        <>
-            <ErrorBoundary>
-                <RN.ScrollView style={{flex: 1}} contentContainerStyle={{paddingBottom: 38}}>
-                    <FormSection title="Show in" titleStyleType="no_border">
-                        <FormSwitchRow
-                            label="User profile"
-                            subLabel="Show invite button in user profile. When you click you send a friend invite link to selected user as message"
-                            leading={<FormRow.Icon source={getAssetIDByName("ic_members")} />}
-                        />
-                        <FormSwitchRow
-                            label="Friend Tab"
-                            subLabel="Show invite button as toolbox in friends tab"
-                            leading={<FormRow.Icon source={getAssetIDByName("friends_toast_icon")} />}
-                        />
-                    </FormSection>
-                </RN.ScrollView>
-            </ErrorBoundary>
-        </>
-    )
-}
+export default () => (
+    <RN.ScrollView style={{flex: 1}} contentContainerStyle={{paddingBottom: 38}}>
+        <FormSection title="Show in" titleStyleType="no_border">
+            <FormSwitchRow
+                label="User profile"
+                subLabel="Show invite button in user profile. When you click you send a friend invite link to selected user as message"
+                leading={<FormRow.Icon source={getAssetIDByName("ic_members")} />}
+            />
+            <FormSwitchRow
+                label="Friend Tab"
+                subLabel="Show invite button as toolbox in friends tab"
+                leading={<FormRow.Icon source={getAssetIDByName("friends_toast_icon")} />}
+            />
+        </FormSection>
+    </RN.ScrollView>
+)
