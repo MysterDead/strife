66876c36f7a862df3d118ce032c2543070c83720
MysterDead <bobryk20@gmail.com>
2023-02-28 15:27:37 +0100

    test

--- plugins/FriendLink/index.tsx
+++ plugins/FriendLink/index.tsx
@@ -4,31 +4,33 @@ import { storage } from "@vendetta/plugin";
 import {findByDisplayName, findByProps, } from "@vendetta/metro";
 import {after} from "@vendetta/patcher";
 import {Forms} from "@vendetta/ui/components";
+import {findInReactTree} from "@vendetta/utils";
 
 storage.profileButton ??=false;
 storage.friendsTabButton ??=false;
 let unpatch;
 
-const UserProfileActions = findByDisplayName("UserProfileActions", false);
+const UserProfileExperimentWrapper = findByDisplayName("UserProfileExperimentWrapper", false);
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 
 
-const UnpatchRelations = after('default', UserProfileActions, ([{user}], res) => {
-    logger.log('TEST - '+JSON.stringify(user));
-    let buttons = res?.props?.children?.props?.children?.props?.children[1];
-    const buttonCallback = () => {
-        console.log("I was clicked!");
-        // You can use LazyActionSheet's hideActionSheet function to close the action sheet.
-        LazyActionSheet.hideActionSheet();
-    };
-    buttons.push((<Forms.FormRow
-        icon={105}
-        iconColor={'#b8b9bf'}
-        label={'Friend Link'}
-        onPress={buttonCallback}
-        accessibilityHint={'Send a friend link'}
-        textColor={'#1f7026'}
-    />));
+const UnpatchRelations = after('default', UserProfileExperimentWrapper, ([{user}], res) => {
+    const UserProfileActions = findInReactTree(res.props.children, i => i.type && i.type.name === "UserProfileActions");
+    after("render", UserProfileActions.type.prototype, (_, { props: { children } }) => {
+        const buttonCallback = () => {
+            console.log("I was clicked!");
+            // You can use LazyActionSheet's hideActionSheet function to close the action sheet.
+            LazyActionSheet.hideActionSheet();
+        };
+        children.push((<Forms.FormRow
+            icon={105}
+            iconColor={'#b8b9bf'}
+            label={'Friend Link'}
+            onPress={buttonCallback}
+            accessibilityHint={'Send a friend link'}
+            textColor={'#1f7026'}
+        />));
+    })
 });
 
 export default {
