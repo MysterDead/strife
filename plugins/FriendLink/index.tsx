fb4393a54d0379d67c197408305869a829d4d2a3
MysterDead <bobryk20@gmail.com>
2023-03-01 19:04:53 +0100

    last of findByDisplayName

--- plugins/FriendLink/index.tsx
+++ plugins/FriendLink/index.tsx
@@ -15,7 +15,7 @@ const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 
 
 
-const UnpatchRelations = after('default', UserProfileRelations.user, (ctx, component) => {
+const UnpatchRelations = after('default', UserProfileRelations, (ctx, component) => {
     console.log("ABC - CTX - I see the element");
     // const { props } = component;
     // const { child } = props;
