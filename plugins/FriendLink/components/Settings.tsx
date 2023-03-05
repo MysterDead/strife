4ae36007b0ab7f24b57688bdc62d1e3b60a2c098
MysterDead <bobryk20@gmail.com>
2023-03-05 10:37:28 +0100

    ss

--- plugins/FriendLink/components/Settings.tsx
+++ plugins/FriendLink/components/Settings.tsx
@@ -1,14 +1,16 @@
 /// <reference path="..\..\..\node_modules\vendetta-types\defs.d.ts" />
 
 import { ReactNative as RN } from "@vendetta/metro/common";
-import {ErrorBoundary, Forms} from "@vendetta/ui/components";
-import {getAssetByID, getAssetIDByName} from "@vendetta/ui/assets";
-const { FormSection, FormSwitchRow, FormRow } = Forms;
+import {Forms} from "@vendetta/ui/components";
+import {getAssetIDByName} from "@vendetta/ui/assets";
+const { FormSection, FormSwitchRow, FormRow, FormInput } = Forms;
 import { storage } from "@vendetta/plugin";
 import {useProxy} from "@vendetta/storage";
+import { toasts } from "@vendetta/ui";
 
 export default () => {
     useProxy(storage);
+    storage.messageContent = ':wave: **Hi there!**\nPlease add me to your friends list!';
     return (
         <RN.ScrollView style={{flex: 1}} contentContainerStyle={{paddingBottom: 38}}>
             <FormSection title="General" titleStyleType="no_border">
@@ -16,11 +18,14 @@ export default () => {
                     label="Invites History"
                     subLabel="Show invites list history"
                     leading={<FormRow.Icon source={getAssetIDByName("ic_history_24px")} />}
+                    onPress={() => {
+                        toasts.showToast('Invite history is not implemented yet', 705);
+                    }}
                 />
                 <FormRow
-                    label="Generate Invite"
-                    subLabel="Open modal to generate invite from this place"
-                    leading={<FormRow.Icon source={getAssetIDByName("ic_audit_log_24px")} />}
+                    label="Message content with Invite Link. (Preview)"
+                    subLabel={storage.messageContent}
+                    leading={<FormRow.Icon source={getAssetIDByName("ic_selection_checked_24px")} />}
                 />
             </FormSection>
             <FormSection title="Show in" titleStyleType="no_border">
@@ -46,7 +51,7 @@ export default () => {
                 />
                 <FormRow
                     label="Version"
-                    subLabel="Alpha - 0.1"
+                    subLabel="Alpha - 0.2"
                     leading={<FormRow.Icon source={getAssetIDByName("ic_selection_checked_24px")} />}
                 />
             </FormSection>
